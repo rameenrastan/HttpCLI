771a73226315e26dce4090f6026956f73a067c81
rameenrastan <rameenrastanv@hotmail.com>
2017-10-05 21:31:14 -0400

    python 3 fix

--- get.py
+++ get.py
@@ -8,7 +8,7 @@ s = socket.socket()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     
 
 s.connect(('www.cnn.com', 80))
-s.sendall("GET / HTTP/1.1\r\nHost: www.cnn.com\r\nContent-Type: application/json\r\n\r\n")
+s.sendall(b"GET / HTTP/1.1\r\nHost: www.cnn.com\r\nContent-Type: application/json\r\n\r\n")
 
 print(s.recv(4096))
 s.close
